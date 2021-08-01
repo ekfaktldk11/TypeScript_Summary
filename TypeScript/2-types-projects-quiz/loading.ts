59d49f65814fd7beb3ccd085f3ce92f96a30edf4
ekfaktldk11 <ekfaktldk11@naver.com>
2021-08-01 16:59:43 +0900

    end of chap 3

--- TypeScript/2-types-projects-quiz/loading.ts
+++ TypeScript/2-types-projects-quiz/loading.ts
@@ -20,6 +20,22 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(loadInfo: ResourceLoadState) {
+    switch (loadInfo.state) {
+      case 'loading':
+        console.log('loading...');
+        break;
+      case 'success':
+        console.log(loadInfo.response.body);
+        break
+      case 'fail':
+        console.log(loadInfo.reason);
+        break
+      default:
+        throw new Error(`unknown state: ${loadInfo}`);
+    }
+  };
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
