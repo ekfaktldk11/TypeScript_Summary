d7b9a7539386aee6bb1625f2112dc9029fc4fee7
ekfaktldk11 <ekfaktldk11@naver.com>
2021-10-22 17:58:07 +0900

    polymorphism

--- TypeScript/3-oop/3-6-polymorphism.ts
+++ TypeScript/3-oop/3-6-polymorphism.ts
@@ -2,13 +2,14 @@
     // Encapsultaion
     type CoffeeCup = {
         shots: number;
-        hasMilk: boolean;
+        hasMilk?: boolean; // optional - hasSome 이 있을 수 도 있고 없을 수 도 있고 ~
+        hasSugar?: boolean;
     };
 
     /*
-        inheritance ( 상속 )
-        - class A extends A_ {}
-        - super()
+        polymorphism (다형성)
+        - 다형성을 이용하면 한가지의 클래스나 
+        한가지의 인터페이스로 다른 방식으로 구현한 클래스를 만들 수 있음!
     */
     
     // 커피를 만드는 기능만을 보유한 인터페이스
@@ -20,7 +21,6 @@
         private static BEANS_GRAM_PER_SHOT: number = 7; // class level
         private coffeeBeans: number = 0; // instance (object) level
 
-        // 생성자가 private 이면 상속불가 -> protected(상속 받은 놈들은 수정가능) or public 사용
         constructor(coffeeBeans: number) {
             this.coffeeBeans = coffeeBeans;
 
@@ -43,10 +43,10 @@
 
         private gridBeans(shots : number){
             console.log(`grinding beans for ${shots}`);
-            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT){ // 커피콩 부족
+            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT){
                 throw new Error('Not enough coffee beans!');
             }
-            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT; // 갈고있는 커피콩만큼 제거
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
         }
 
         private preheat(): void{
@@ -69,10 +69,6 @@
     }
 
     class CaffeeLatteMachine extends CoffeeMachine{
-        /*
-            만약 자식클래스에서 생성자를 따로 구현하고 싶은 경우엔
-            부모의 생성자도 호출해줘야함. 생성자는 따로 함수가 아니기에 그냥 'super(some args)' 만 작성하면됨
-        */
         constructor(beans: number, public readonly serialNumber: string){ // readonly : 한번 설정하면 바뀌지 않는 값들 앞에 붙여줌
             super(beans);
         }
@@ -96,6 +92,41 @@
             }
         }
     }
+    class SweetCoffeeMaker extends CoffeeMachine {
+        makeCoffee(shots: number): CoffeeCup {
+            const coffee = super.makeCoffee(shots);
+            return {
+                ...coffee,
+                hasSugar: true
+            }
+        }
+    }
+    /*
+        SweetCoffeeMaker&CaffeeLatteMachine 은 CoffeeMachine 이고
+        CoffeeMachine 은 CoffeeMaker 라는 인터페이스를 구현하는 클래스 이다.
+        따라서! SweetCoffeeMaker&CaffeeLatteMachine 도 CoffeeMaker 라는 인터페이스를 구현하는 클래스 이다.
+    */
+
+    // 위 주석의 특성을 이용하여 CoffeeMaker[] 타입으로 생성가능
+    const machines: CoffeeMaker[] = [
+        new CoffeeMachine(16),
+        new CaffeeLatteMachine(16, '1'),
+        new SweetCoffeeMaker(16),
+        new CoffeeMachine(16),
+        new CaffeeLatteMachine(16, '1'),
+        new SweetCoffeeMaker(16)
+    ]
+    // machines 은 CoffeeMaker[] 타입이므로 CoffeeMaker 인터페이스에 규약된 makeCoffee() 만 사용가능
+    machines.forEach(machine => {
+        console.log('---------------------');
+        machine.makeCoffee(1);
+    });
+    // 여기서 다형성 최고 장점!!
+    /*
+        내부적으로 구현된 다양한 클래스들이 한가지 인터페이스를 구현하거나
+        또는 동일한 부모클래스를 상속했을 때 위처럼 동일한 함수를 어떤 클래스인지 구분하지 않고
+        공통된 API를 호출할 수 있는것이 가장 큰 장점 !
+    */
 
     //const machine = new CoffeeMachine(23);
     const latteMachine = new CaffeeLatteMachine(23, 'SSSS');
